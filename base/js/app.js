d04fb0779718f71af29ee2c0a292602be1274a7b
blibotics <blibotics@gmail.com>
2013-10-07 23:27:54 -0400

    Appending text to each li and appending the li to the ul via js

--- base/js/app.js
+++ base/js/app.js
@@ -1,9 +1,12 @@
 var sectionContentWrapper = document.getElementById('content');
 var sections = sectionContentWrapper.getElementsByTagName('div');
+var nav = document.getElementById('nav');
 
 for (i = 0; i < sections.length; i++) {
   var section = sections[i];
   var title = section.getElementsByTagName('h3')[0].innerText;
 
-  console.log(title);
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode(title));
+  nav.appendChild(li); 
 }
