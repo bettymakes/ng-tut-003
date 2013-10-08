505c624d36f9869526d15976980422a203a6fc34
blibotics <blibotics@gmail.com>
2013-10-07 23:34:55 -0400

    Creating base for calculating position of each element and creating a function that fires on scroll

--- base/js/app.js
+++ base/js/app.js
@@ -2,6 +2,7 @@ var sectionContentWrapper = document.getElementById('content');
 var sections = sectionContentWrapper.getElementsByTagName('div');
 var nav = document.getElementById('nav');
 
+// Loop to append titles to the nav
 for (i = 0; i < sections.length; i++) {
   var section = sections[i];
   var title = section.getElementsByTagName('h3')[0].innerText;
@@ -9,4 +10,14 @@ for (i = 0; i < sections.length; i++) {
   var li = document.createElement('li');
   li.appendChild(document.createTextNode(title));
   nav.appendChild(li); 
+}
+
+// Calculates the position of each section
+positionCalculator = function(){
+  console.log('hero');
+};
+
+// Triggers positionCalculator function
+window.onscroll = function(){
+  positionCalculator();
 }
